f0707401d6d0a2bd1617259118709ae3518e9afc
mokev10 <mokev10@outlook.com>
2026-04-09 15:30:51 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     name="calcul-dl-california",
     version="0.1.0",
     description="PDF417 / AAMVA tools and Streamlit demo",
-    packages=find_packages(include=["pdf417gen", "pdf417gen.*"]),
+    packages=find_packages(include=["pdf417gen", "pdf417en.*"]),
     install_requires=[
         "requests>=2.28",
         "reportlab>=4.0",
